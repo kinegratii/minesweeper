41e201fbb0247defe168cf0e3ca8628423be3334
kinegratii <kinegratii@gmail.com>
2015-06-10 10:03:15 +0800

    format py2compat module

--- minesweeper/py2compat.py
+++ minesweeper/py2compat.py
@@ -1,8 +1,15 @@
 # coding=utf8
-
+from __future__ import unicode_literals
 import sys
 
-__all__ = ('range', 'reduce', 'queue', 'open', 'tkinter', 'messagebox')
+__all__ = [
+    'range',
+    'reduce',
+    'queue',
+    'open',
+    'tkinter',
+    'messagebox',
+]
 
 PY2 = sys.version_info[0] == 2
 PY3 = sys.version_info[0] == 3
@@ -13,13 +20,9 @@ if PY2:
     import Queue as queue
     from io import open
     import Tkinter as tkinter
-    import tkMessageBox
-    messagebox = tkMessageBox
+    import tkMessageBox as messagebox
 
 else:
     range = range
     from functools import reduce
-    import queue
     from io import open
-    import tkinter
-    from tkinter import messagebox
